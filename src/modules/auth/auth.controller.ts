6b5a55c1b92d334e1b899a0387ff086dd9ec4482
kuzmrom7 <kuzmrom7@gmail.com>
2025-01-16 18:51:23 +0300

    refactor auth.controller.ts

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -14,80 +14,67 @@ type LoginEmailAccountValidateCodeBody = {
   email: string;
 };
 
-export async function refreshAccessToken(req: FastifyRequest, reply: FastifyReply) {
-  const { refreshToken } = req.cookies;
+export default {
+  async refreshAccessToken(req: FastifyRequest, reply: FastifyReply) {
+    const { refreshToken } = req.cookies;
 
-  if (!refreshToken) return reply.code(401).send({ message: 'Refresh token not found' });
+    if (!refreshToken) return reply.code(401).send({ message: 'Refresh token not found' });
 
-  try {
     const decoded = await decodeToken(req.jwt, refreshToken);
     const newAccessToken = await generatedAccessToken(req.jwt, decoded.id, decoded.email);
 
     reply.send({ token: newAccessToken });
-  } catch (err) {
-    console.log(err);
-    reply.code(401).send({ message: 'Invalid refresh token' });
-  }
-}
+  },
+  async loginEmail(req: FastifyRequest<{ Body: LoginEmailAccountBody }>, reply: FastifyReply) {
+    const { email } = req.body;
+    const generatedCode =
+      config.nodeEnv === 'development' ? Number(config.dev.authCode) : Math.floor(Math.random() * 100000);
+    await saveEmailCode(email, generatedCode);
 
-export async function loginEmail(req: FastifyRequest<{ Body: LoginEmailAccountBody }>, reply: FastifyReply) {
-  const { email } = req.body;
-  const generatedCode =
-    config.nodeEnv === 'development' ? Number(config.dev.authCode) : Math.floor(Math.random() * 100000);
-  await saveEmailCode(email, generatedCode);
-  try {
     if (config.nodeEnv !== 'development') {
-      sendEmailCode(email, generatedCode);
+      await sendEmailCode(email, generatedCode);
     }
-  } catch (e) {
-    console.log(e);
-    return reply.code(400).send({ message: JSON.stringify(e) });
-  }
 
-  reply.code(200).send('ok');
-}
+    reply.code(200).send('ok');
+  },
+  async loginEmailValidateCode(req: FastifyRequest<{ Body: LoginEmailAccountValidateCodeBody }>, reply: FastifyReply) {
+    const { email, code } = req.body;
 
-export async function loginEmailValidateCode(
-  req: FastifyRequest<{ Body: LoginEmailAccountValidateCodeBody }>,
-  reply: FastifyReply
-) {
-  const { email, code } = req.body;
+    const emailCode = await getEmailCode(email);
+    if (!emailCode) {
+      return reply.code(400).send({ message: 'Code not found' });
+    }
 
-  // 1. verify code
-  const emailCode = await getEmailCode(email);
-  if (!emailCode) {
-    return reply.code(400).send({ message: 'Code not found' });
-  }
+    if (emailCode !== Number(code)) {
+      return reply.code(400).send({ message: 'Code not match' });
+    }
 
-  if (emailCode !== Number(code)) {
-    return reply.code(400).send({ message: 'Code not match' });
-  }
+    const account = await createAccount({ email });
+    await deleteEmailCode(email);
 
-  const account = await createAccount({ email });
-  await deleteEmailCode(email);
+    if (!account) {
+      return reply.code(409).send();
+    }
 
-  if (!account) {
-    return reply.code(409).send();
-  }
+    const accessToken = await generatedAccessToken(req.jwt, account.id, account.email);
+    const refreshToken = await generatedRefreshToken(req.jwt, account.id, account.email);
 
-  const accessToken = await generatedAccessToken(req.jwt, account.id, account.email);
-  const refreshToken = await generatedRefreshToken(req.jwt, account.id, account.email);
-
-  reply
-    .setCookie('refreshToken', refreshToken, {
-      httpOnly: true,
-      secure: false,
-      path: '/',
-      sameSite: 'strict',
-      maxAge: 604800, // 7 days
-    })
-    .code(200)
-    .send({
-      token: accessToken,
-      account: {
-        id: account?.id,
-        email: account?.email,
-        phone: account?.phone,
-      },
-    });
-}
+    reply
+      .setCookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: false,
+        path: '/',
+        sameSite: 'strict',
+        maxAge: 604800, // 7 days
+      })
+      .code(200)
+      .send({
+        token: accessToken,
+        account: {
+          id: account?.id,
+          email: account?.email,
+          phone: account?.phone,
+        },
+      });
+  },
+};
